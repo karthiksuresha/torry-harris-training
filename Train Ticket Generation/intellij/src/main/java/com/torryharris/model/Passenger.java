4d21e19a78ff323397cb8252d3c05269e7d1f25d
karthiksuresha <89825873+karthiksuresha@users.noreply.github.com>
2021-11-19 14:22:51 +0530

    Add files via upload

---
+++ Train Ticket Generation/intellij/src/main/java/com/torryharris/model/Passenger.java
@@ -0,0 +1,70 @@
+package com.torryharris.model;
+//Passenger POJO Class
+public class Passenger implements Comparable<Passenger>{
+    String name;
+    int age;
+    char gender;
+    double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Passenger(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public Passenger(String name, int age, char gender,double price) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Passenger() {
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Passenger o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
